8bcf899ef23d8790bc08a70d99c1b2561aaf4295
= <=>
2023-07-08 20:13:58 -0300

    creando estado local en componente search para capturar value del e.target

--- src/components/TodoSearch/TodoSearch.jsx
+++ src/components/TodoSearch/TodoSearch.jsx
@@ -1,11 +1,16 @@
-import React from 'react'
+import React, { useState }from 'react'
 import './TodoSearch.css'
 
 export default function TodoSearch() {
+  const [searchValue , setSearchValue] = useState('');
   return (
     <input 
     className='TodoSearch'
     placeholder='cortar cebolla'
+    value={searchValue}
+    onChange={(e)=>{
+      setSearchValue(e.target.value)
+    }}
     />
   )
 }
