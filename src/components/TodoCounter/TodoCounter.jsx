7b08ac444ccf2859c0ae66c5afc50651912deb91
= <=>
2023-07-09 07:23:07 -0300

    instalando dependencias de iconos para mejorar estilos de la interface

--- src/components/TodoCounter/TodoCounter.jsx
+++ src/components/TodoCounter/TodoCounter.jsx
@@ -2,8 +2,12 @@ import React from 'react'
 import './TodoCounter.css'
 
 export default function TodoCounter({total,completed}) {
-
-  return (
-    <h1 className='todoCounter'>Has completado <span>{completed}</span> de <span>{total}</span> TODOS</h1>
-  )
+  if(total === completed){
+    return(
+      <h1 className='todoCounter'>Felicidades!!! Has completado todas las tareas</h1>
+    )}else{
+      return (
+        <h1 className='todoCounter'>Has completado <span>{completed}</span> de <span>{total}</span> TODOS</h1>
+      )
+  }
 }
--- src/components/TodoItem/TodoItem.jsx
+++ src/components/TodoItem/TodoItem.jsx
@@ -2,16 +2,16 @@ import './TodoItem.css'
 export default function TodoItem(props) {
     return (
       <li className='TodoItem'>
-        <span className={`Icon Icon-check ${props.completed && 'Icon-check--active'}`}
+        {/* <span className={`Icon Icon-check ${props.completed && 'Icon-check--active'}`}
         onClick={props.onComplete}>
           V{props.completed}
-        </span>
+        </span> */}
         <p className={`TodoItem-p ${props.completed &&'TodoItem-p--complete'}`}>{props.text}</p>
-        <span className={`Icon Icon-delete`}
+        {/* <span className={`Icon Icon-delete`}
         onClick={props.onDelete}
         >
           X
-        </span>
+        </span> */}
       </li>
     );
   }
