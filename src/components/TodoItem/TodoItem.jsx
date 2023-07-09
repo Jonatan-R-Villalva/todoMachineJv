c5d952ba0fe46807be92744224fef78942a9f105
= <=>
2023-07-09 08:08:41 -0300

    remplazando span por iconos en items para completar y elimnar tareas

--- src/components/TodoItem/TodoItem.jsx
+++ src/components/TodoItem/TodoItem.jsx
@@ -1,17 +1,19 @@
 import './TodoItem.css'
+import { FaCheck } from 'react-icons/fa'
+import { AiOutlineClose } from 'react-icons/ai'
 export default function TodoItem(props) {
     return (
       <li className='TodoItem'>
-        {/* <span className={`Icon Icon-check ${props.completed && 'Icon-check--active'}`}
+        <span className={`Icon Icon-check ${props.completed && 'Icon-check--active'}`}
         onClick={props.onComplete}>
-          V{props.completed}
-        </span> */}
+          <FaCheck />{props.completed}
+        </span>
         <p className={`TodoItem-p ${props.completed &&'TodoItem-p--complete'}`}>{props.text}</p>
-        {/* <span className={`Icon Icon-delete`}
+        <span className={`Icon Icon-delete`}
         onClick={props.onDelete}
         >
-          X
-        </span> */}
+          <AiOutlineClose />
+        </span>
       </li>
     );
   }
